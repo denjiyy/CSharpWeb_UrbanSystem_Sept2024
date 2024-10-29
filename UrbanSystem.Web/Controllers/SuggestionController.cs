646f4e916b7aaa011213de31130b53093e6a6a81
lainogovno <tabutovdenis@gmail.com>
2024-10-29 22:06:24 +0200

    Added views for the suggestion

---
+++ UrbanSystem.Web/Controllers/SuggestionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UrbanSystem.Data;
+
+namespace UrbanSystem.Web.Controllers
+{
+    public class SuggestionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SuggestionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var suggestions = await _context.Suggestions
+                .ToListAsync();
+
+            return View(suggestions);
+        }
+    }
+}
