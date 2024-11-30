81f782df4acb3ff215880ed40c70c02ead15ec35
Denis Tabutov <125385668+denjiyy@users.noreply.github.com>
2024-11-30 17:41:30 +0200

    Delete UrbanSystem.Common directory

--- UrbanSystem.Common/ApplicationConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UrbanSystem.Common
-{
-    public static class ApplicationConstants
-    {
-        public const int ReleaseYear = 2024;
-
-        public const string AdminRoleName = "Admin";
-    }
-}
