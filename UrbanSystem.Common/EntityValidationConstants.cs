60be1bc674e4e9bb6953dafc8efc71e2d9019a07
Denis Tabutov <125385668+denjiyy@users.noreply.github.com>
2024-12-11 01:31:12 +0200

    Add files via upload

--- UrbanSystem.Common/EntityValidationConstants.cs
+++ UrbanSystem.Common/EntityValidationConstants.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UrbanSystem.Common
+﻿namespace UrbanSystem.Common
 {
     public static class EntityValidationConstants
     {
